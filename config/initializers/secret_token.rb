42e6c485eedba6de217a4ddee184d7e293d9ab1b
Nick Quaranto <nick@quaran.to>
2011-03-31 16:05:35 -0400

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArelTest::Application.config.secret_token = '05995067b8fcdb3eeed7300c3bd663bc8b9ce855ce6e912a07bfa4b842ce4e753de8fb41289d673f79c45fa127bb987b7052f90a220206a9f4f3e74991bc5110'
